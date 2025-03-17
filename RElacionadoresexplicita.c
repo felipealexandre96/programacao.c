3c76d3e42f1df6f8263e79d9088453a3c81bb018
felipealexandre96 <felipealexandrebarbosa@gmail.com>
2025-03-17 15:58:55 +0000

    operadores relacionais

---
+++ RElacionadoresexplicita.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    float numero1 = 10.2;
+    int numero2 = 10;
+
+    printf("Numero1 > Numero2: %d\n", (int)numero1 > numero2);
+    printf("Numero1 == Numero2: %d\n", (int)numero1 == numero2);
+
+
+    return 0;
+}
