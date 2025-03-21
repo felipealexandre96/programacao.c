e037ad639827a7c3b65cff62f8a7fee67e3fb039
felipealexandre96 <felipealexandrebarbosa@gmail.com>
2025-03-21 15:33:31 +0000

    Estruturas de Decisão Simples

---
+++ verificaçãodeidade.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+    int main(){
+        int idade1, idade2;
+        idade1 = 25;
+        idade2 = 30;
+
+    if(idade1 < idade2){
+        printf("Pessoa 1 é mais nova que Pesoa 2\n");
+
+    }
+    }
