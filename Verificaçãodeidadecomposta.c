885a11ac1e429f243025d3c1238a55636e8d21d5
felipealexandre96 <felipealexandrebarbosa@gmail.com>
2025-03-24 14:59:18 +0000

    mais uma atualização

--- Verificaçãodeidadecomposta.c
+++ Verificaçãodeidadecomposta.c
@@ -3,12 +3,14 @@
         /*se a idade for 18 ou mais, o programa imprime 'você é maior de idade'.
           No nosso exemplo, 20 é maior que 18, então a mensagem será exibida*/
 
-          int idade = 17;
+          int idade = 50;
 
           if(idade >= 18) {
+            //condição verdadeira
             printf("Você é maior de idade\n");
-            printf("Dentro IF\n");
+          } else {
+            //condição falsa
+            printf("Você é menor de idade\n");
           }
-            printf("Fora IF");
           
     }
