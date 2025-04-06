ddeb42740c2f079e7166711388e4ca0d8caf31ea
felipealexandre96 <felipealexandrebarbosa@gmail.com>
2025-04-06 14:06:21 +0000

    recursividade e procedimentos

---
+++ Procedimentos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//Definição de procedimento para imprimir uma mensagem
+void imprimirmensagem(){
+    printf("Olá, mundo!\n");
+}
+
+int main(){
+    //Chamada do procedimento
+    imprimirmensagem();
+    return 0;
+}
